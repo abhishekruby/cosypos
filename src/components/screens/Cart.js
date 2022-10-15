d0175a0b2a1e32ab005681c6e6fb2bc3bdc1deaf
abhishekruby <abhishekruby94@gmail.com>
2022-10-15 10:12:02 +0530

    Cart section struction started

--- src/components/screens/Cart.js
+++ src/components/screens/Cart.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
 function Cart() {
   return (
-    
-  )
+    <Cart>
+      <Wrapper>
+        <Container>
+          <Head>
+            <Heading>
+              <Title>Table 5</Title>
+              <Name>Leslie k</Name>
+              <Icon>
+                <IconImage src={require("../assets/images/").default} />
+              </Icon>
+            </Heading>
+            <CartContainer>
+              <ItemList>
+                <Item>
+                  <FlexContainer>
+                    <Number></Number>
+                    <Title></Title>
+                    <Price></Price>
+                  </FlexContainer>
+                </Item>
+              </ItemList>
+            </CartContainer>
+            TotalContainer
+          </Head>
+        </Container>
+      </Wrapper>
+    </Cart>
+  );
 }
 
-export default Cart
+export default Cart;
