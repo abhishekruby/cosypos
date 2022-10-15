d996eea3dbf7ff39de818d6a8d2d5b1aa0abe28e
abhishekruby <abhishekruby94@gmail.com>
2022-10-15 12:15:32 +0530

    Cart section styling started

--- src/components/screens/Cart.js
+++ src/components/screens/Cart.js
@@ -9,68 +9,67 @@ function Cart() {
           <Head>
             <Heading>
               <Title>Table 5</Title>
-              <Name>Leslie k</Name>
+              <Name>Leslie k.</Name>
             </Heading>
             <Icon>
               <IconImage src={require("../assets/images/edit.svg").default} />
             </Icon>
-
-            <ItemContainer>
-              <ItemList>
-                <Item>
-                  <FlexContainer>
-                    <Number>1</Number>
-                    <ItemTitle>Roast chicken</ItemTitle>
-                    <Price>$25.50</Price>
-                  </FlexContainer>
-                </Item>
-              </ItemList>
-            </ItemContainer>
-
-            <TotalContainer>
-              <TopSection>
-                <SubTotal>
-                  <SubTitle>Subtotal</SubTitle>
-                  <SubPrice>$171.5</SubPrice>
-                </SubTotal>
-                <Tax>
-                  <TaxTitle>Tax 10%</TaxTitle>
-                  <TaxPrice>$17.15</TaxPrice>
-                </Tax>
-                <Line />
-                <Total>
-                  <TotalTitle>Total</TotalTitle>
-                  <TotalPrice>$2883.5</TotalPrice>
-                </Total>
-              </TopSection>
-              <BottomSection>
-                <PaymentMethod>
-                  <PaymentTitle>Payment Method</PaymentTitle>
-                  <PaymentOption>
-                    <Cash>
-                      <CashImage
-                        src={require("../assets/images/dollar.svg").default}
-                      />
-                      <CashTitle>Cash</CashTitle>
-                    </Cash>
-                    <DebitCard>
-                      <CardImage
-                        src={require("../assets/images/credit.svg").default}
-                      ></CardImage>
-                      <CardTitle>Debit Card</CardTitle>
-                    </DebitCard>
-                    <Wallet>
-                      <WalletImage
-                        src={require("../assets/images/scan.svg").default}
-                      ></WalletImage>
-                      <WalletTitle>E-wallet</WalletTitle>
-                    </Wallet>
-                    <PlaceOrdered>Place Order</PlaceOrdered>
-                  </PaymentOption>
-                </PaymentMethod>
-              </BottomSection>
-            </TotalContainer>
           </Head>
+          <ItemContainer>
+            <ItemList>
+              <Item>
+                <FlexContainer>
+                  <Number>1</Number>
+                  <ItemTitle>Roast chicken</ItemTitle>
+                  <Price>$25.50</Price>
+                </FlexContainer>
+              </Item>
+            </ItemList>
+          </ItemContainer>
+
+          <TotalContainer>
+            <TopSection>
+              <SubTotal>
+                <SubTitle>Subtotal</SubTitle>
+                <SubPrice>$171.5</SubPrice>
+              </SubTotal>
+              <Tax>
+                <TaxTitle>Tax 10%</TaxTitle>
+                <TaxPrice>$17.15</TaxPrice>
+              </Tax>
+              <Line />
+              <Total>
+                <TotalTitle>Total</TotalTitle>
+                <TotalPrice>$2883.5</TotalPrice>
+              </Total>
+            </TopSection>
+            <BottomSection>
+              <PaymentMethod>
+                <PaymentTitle>Payment Method</PaymentTitle>
+                <PaymentOption>
+                  <Cash>
+                    <CashImage
+                      src={require("../assets/images/dollar.svg").default}
+                    />
+                    <CashTitle>Cash</CashTitle>
+                  </Cash>
+                  <DebitCard>
+                    <CardImage
+                      src={require("../assets/images/credit.svg").default}
+                    ></CardImage>
+                    <CardTitle>Debit Card</CardTitle>
+                  </DebitCard>
+                  <Wallet>
+                    <WalletImage
+                      src={require("../assets/images/scan.svg").default}
+                    ></WalletImage>
+                    <WalletTitle>E-wallet</WalletTitle>
+                  </Wallet>
+                  <PlaceOrdered>Place Order</PlaceOrdered>
+                </PaymentOption>
+              </PaymentMethod>
+            </BottomSection>
+          </TotalContainer>
         </Container>
       </Wrapper>
     </CartContainer>
@@ -79,24 +78,58 @@ function Cart() {
 
 export default Cart;
 
-const CartContainer = styled.aside``;
+const CartContainer = styled.aside`
+  width: 30%;
+  padding: 10px 0px;
+`;
 const Wrapper = styled.div``;
 const Container = styled.div``;
-const Head = styled.div``;
-const Heading = styled.div``;
-const Title = styled.h3``;
-const Name = styled.blockquote``;
-const Icon = styled.div``;
-const IconImage = styled.img``;
+const Head = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Heading = styled.div`
+  width: 40%;
+`;
+const Title = styled.h3`
+  font-size: 22px;
+  font-weight: 500;
+`;
+const Name = styled.blockquote`
+  font-size: 15px;
+  color: #a6a6a6;
+`;
+const Icon = styled.div`
+  background: #fff;
+  border-radius: 50%;
+  width: 13%;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const IconImage = styled.img`
+  width: 60%;
+  height: 60%;
+`;
 
 const ItemContainer = styled.div``;
 const ItemList = styled.ul``;
-const Item = styled.li``;
-const FlexContainer = styled.div``;
-const Number = styled.span``;
-const ItemTitle = styled.h4``;
-const Price = styled.span``;
-
+const Item = styled.li`
+  border-radius: 8px;
+  background: #2d2d2d;
+`;
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Number = styled.span`
+display:inline-block;
+width: 5%;`;
+const ItemTitle = styled.h4`
+width: 60%;`;
+const Price = styled.span`
+width: 20%;`;
 
 const TotalContainer = styled.div``;
 const TopSection = styled.div``;
